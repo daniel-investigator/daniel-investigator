c2467050d592fc6ca3f4e646b61773c3d4fe511a
matt investigator <mattinvestigator@gmail.com>
2020-03-29 10:26:09 -0500

    Objetos iniciales

---
+++ src/test/java/com/initial/stabilizer/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initial.stabilizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
